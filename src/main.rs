17ce61428bc01b3e4217dadc9a374eb2811eee37
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2021-01-02 14:41:33 +0800

    use expect

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::env::args;
 use regex::Regex;
 use reqwest::header::USER_AGENT;
 use select::document::Document;
@@ -8,41 +8,35 @@ use std::fs::File;
 
 fn download_link(url: &str, file_name: &str) {
     println!("Downloading {}...", url);
-    let mut resp = reqwest::blocking::get(url).expect("request failed");
-    let mut out = File::create(format!("{}", file_name)).expect("failed to create file");
-    io::copy(&mut resp, &mut out).expect("failed to copy content");
+    let mut resp = reqwest::blocking::get(url).expect("Request failed.");
+    let mut out = File::create(format!("{}", file_name)).expect("Unable to create file.");
+    io::copy(&mut resp, &mut out).expect("Unable to copy the content.");
     println!("Done...");
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut args: Vec<String> = env::args().collect();
+fn main() {
+    let url = args()
+        .skip(1)
+        .next().expect("Facebook video URL expected.");
+    
+    let re = Regex::new(r"^https://www.facebook.com/.*").unwrap();
 
-    if args.len() == 2 {
-        let url = args.remove(1);
-        let re = Regex::new(r"^https://www.facebook.com/.*").unwrap();
+    if re.is_match(&url) {
+        let client = reqwest::blocking::Client::new();
+        let res = client.get(&url)
+            .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+            .header("Cookie", "dpr=2; m_pixel_ratio=2; locale=en_US; wd=1675x436")
+            .send()
+            .expect("Request failed.")
+            .text()
+            .expect("Unable to get the body.");
 
-        if re.is_match(&url) {
-            let client = reqwest::blocking::Client::new();
-            let res = client.get(&url)
-                .header(USER_AGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-                .header("Accept-Language", "en-US,en;q=0.9")
-                .header("Cookie", "dpr=2; m_pixel_ratio=2; locale=en_US; wd=1675x436")
-                .send()?
-                .text()?;
-
-            Document::from_read(res.as_bytes())
+        Document::from_read(res.as_bytes())
             .unwrap()
             .find(Attr("property", "og:video:url"))
             .filter_map(|n| n.attr("content"))
             .for_each(|x| download_link(x, "video.mp4"));
-        } else {
-            println!("Invalid URL.");
-        }
-        
     } else {
-        println!("Invalid URL.");
+        panic!("Invalid URL.");
     }
-
-    Ok(())
 }
